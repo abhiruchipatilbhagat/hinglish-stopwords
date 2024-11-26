2bbd40b5fc3532dde63c885cc5de57f6e17bfdd5
Abhiruchi Patil Bhagat <121765345+abhiruchipatilbhagat@users.noreply.github.com>
2024-11-27 02:18:58 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='hinglish-stopwords',
+    name='hinglish_stopwords',
     version='0.1.0',
     description='A simple library to filter hinglish stopwords from text.',
     author='abhiruchipatilbhagat',
