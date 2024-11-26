d96f1530563e51798b11600f31a8820af9f01d41
Abhiruchi Patil Bhagat <121765345+abhiruchipatilbhagat@users.noreply.github.com>
2024-11-27 02:13:50 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,10 @@
+# Stopwords Filter
+
+A simple Python library to filter out stopwords from hinglish text. It comes with a predefined list of stopwords and allows users to add custom stopwords.
+
+## Installation
+
+You can install the library using pip:
+
+```bash
+pip install hinglish-stopwords
---
+++ setup.py
@@ -0,0 +1,20 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name='hinglish-stopwords',
+    version='0.1.0',
+    description='A simple library to filter hinglish stopwords from text.',
+    author='abhiruchipatilbhagat',
+    author_email='abhipatilngp@gmail.com',
+    packages=find_packages(),
+    install_requires=[],
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
